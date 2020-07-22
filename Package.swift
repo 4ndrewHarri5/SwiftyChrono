05c9b9f7951f5ca082b21bc5e4fbb814f1341345
harrisa <andrewharris@rentalcars.com>
2020-07-22 10:40:17 +0100

    updated package to 5.3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-//
-//  Package.swift
-//  SwiftyChrono
-//
-//  Created by Potix on 2017-01-17.
-//  Copyright © 2017 Potix.
-//
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyChrono",
-    dependencies: []
+    dependencies: [],
+    targets:
+        .target(name: "Run", dependencies: [.target(name: "App")]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),s
+        ]
+    ]
 )
